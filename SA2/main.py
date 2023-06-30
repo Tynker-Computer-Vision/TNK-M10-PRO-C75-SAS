9970a461efc4c6b2f511413dfa1730db50b28afe
procodingclass <88050205+procodingclass@users.noreply.github.com>
2023-06-30 10:59:34 +0530

    Update main.py

--- SA2/main.py
+++ SA2/main.py
@@ -51,7 +51,6 @@ print("Number of training ages = ", len(training_ages))
 print("Number of testing ages = ", len(testing_images))
 print(":::::::::::::::::::::::::::::::::::::::::::::::::")
 
-# Create a Sequential model and name it age_model
 age_model = Sequential()
 
 # Add Conv2D layer of 128, kernal size of 3, activation function as relu and input shape as (200,200,3)
@@ -70,13 +69,8 @@ age_model.add(Conv2D(512, kernel_size=3, activation='relu'))
 age_model.add(MaxPool2D(pool_size=3, strides=2))
 
 age_model.add(Flatten())
-
-# Add Dropout of 0.2
 age_model.add(Dropout(0.2))
-
 age_model.add(Dense(512, activation='relu'))
-
-# Add Dense with 1, activation as linear and name as 'age'
 age_model.add(Dense(1, activation='linear', name='age'))
               
 # Compile the model with optimizer 'adam and loss 'name'              
